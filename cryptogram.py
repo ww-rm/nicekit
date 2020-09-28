284074836032a51023c2f14a6d255917e9c01fe8
ww-rm <ww-rm@qq.com>
2020-09-28 17:45:10 +0800

    add RC4

--- cryptogram.py
+++ cryptogram.py
@@ -47,7 +47,7 @@ class RSA:
             bytes_text = b'\x00\x02' + padding_bytes + b'\x00' + bytes_text
             return bytes_text
 
-    def encrypt(self, bytes_text: bytes, padding_type) -> bytes:
+    def encrypt(self, plain_text: bytes, padding_type) -> bytes:
         """
         RSA encrypt
 
@@ -70,17 +70,17 @@ class RSA:
         # padding
         if isinstance(padding_type, str):
             if padding_type == 'zero':
-                padding_text = self._padding_zero(bytes_text)
+                padding_text = self._padding_zero(plain_text)
             elif padding_type == 'zero_reverse':
-                padding_text = self._padding_zero(bytes_text, True)
+                padding_text = self._padding_zero(plain_text, True)
             elif padding_type == 'pkcs1_1':
-                padding_text = self._padding_pkcs1_private(bytes_text)
+                padding_text = self._padding_pkcs1_private(plain_text)
             elif padding_type == 'pkcs1_2':
-                padding_text = self._padding_pkcs1_public(bytes_text)
+                padding_text = self._padding_pkcs1_public(plain_text)
             else:
                 raise ValueError(f"Invalid padding type: {padding_type}")
         elif callable(padding_type):
-            padding_text = padding_type(bytes_text)
+            padding_text = padding_type(plain_text)
         else:
             raise TypeError(f"Invalid padding type: {type(padding_type)}")
 
@@ -91,5 +91,76 @@ class RSA:
 
         return cipher_text
 
-    def decrypt(self, bytes_cipher: bytes) -> bytes:
+    def decrypt(self, cipher_text: bytes) -> bytes:
         ...
+
+
+class RC4:
+    """
+    RC4 stream cryptor
+    """
+    def __init__(self):
+        self.key = None
+        self.table_R = None
+        self.table_S = None
+        self.S_I = 0
+        self.S_J = 0
+
+    def set_key(self, key: bytes):
+        """
+        first use this method to set a key,
+        when called, it will auto invoke self.init_state() to initiate the state
+
+        params:
+            key: used fo encrypt and decrypt
+        """
+
+        self.key = key
+        self.table_R = []
+
+        for i in range(256):
+            self.table_R.append(self.key[i % len(self.key)])
+
+        self.init_state()
+
+    def init_state(self):
+        """
+        initiate the state
+
+        this method will initiate the S table S_I and S_J point,
+        you may call this method when you need to encrypt/decrypt a new message
+        """
+
+        self.S_I = self.S_J = 0
+        self.table_S = list(range(256))
+
+        j = 0
+        for i in range(256):
+            j = (j + self.table_S[i] + self.table_R[i]) % 256
+            self.table_S[i], self.table_S[j] = self.table_S[j], self.table_S[i]
+
+    def encrypt(self, plain_text):
+        """
+        RC4 encrypt
+
+        params:
+            cipher_text: message to decrypt, can be bytes or bytes iterator
+        """
+        return bytes(self._crypt(plain_text))
+
+    def decrypt(self, cipher_text):
+        """
+        RC4 decrypt
+
+        params:
+            cipher_text: message to decrypt, can be bytes or bytes iterator
+        """
+        return bytes(self._crypt(cipher_text))
+
+    def _crypt(self, bytes_text: bytes):
+        for byte in bytes_text:
+            self.S_I = (self.S_I + 1) % 256
+            self.S_J = (self.S_J + self.table_S[self.S_I]) % 256
+            self.table_S[self.S_I], self.table_S[self.S_J] = self.table_S[self.S_J], self.table_S[self.S_I]
+            k = self.table_S[(self.table_S[self.S_I] + self.table_S[self.S_J]) % 256]
+            yield (byte ^ k)
